6c3447649c43ecedc07ab9c7ec36b6ca1c71bb36
Joe Hellerstein <jmh@berkeley.edu>
2012-11-27 11:17:59 -0800

    5. element tests don't support async termination condition tests easily

--- test/element/env.js
+++ test/element/env.js
@@ -1,15 +1,20 @@
-require('./env');
+require('../env');
 
-defaultConsumer = function(expectedResult) {
+defaultConsumer = function(expectedResult, cb) {
   var data = [], arr = [], eosSeen = false;
   return {
     producers: function() { return arr; },
     consume: function(str) { data.push(str); },
     name: function() { return 'consumer'; },
     eosHandled: function() { return eosSeen; },
+    data: function() {return data;},
     consumeEOS: function() {
       eosSeen = true;
       assert.deepEqual(data, expectedResult);
+      if (cb) {
+        var err = null;
+        cb(err, this);
+      }
     }
   };
 };
