f708b14aec2be7ef2a1c99a86a975b487fdaea81
Joe Hellerstein <jmh@berkeley.edu>
2012-12-10 13:07:16 -0800

    Close #11: move defaultConsumer definition up one directory

--- test/env.js
+++ test/env.js
@@ -8,3 +8,23 @@ assert = require('assert');
 getExample = function(ex) {
   return path.resolve(__dirname + '/../examples/data/' + ex);
 };
+
+defaultConsumer = function(expectedResult, cb) {
+  var data = [], arr = [], eosSeen = false;
+  return {
+    producers: function() { return arr; },
+    consume: function(str) { data.push(str); },
+    name: function() { return 'consumer'; },
+    eosHandled: function() { return eosSeen; },
+    data: function() {return data;},
+    consumeEOS: function() {
+      eosSeen = true;
+      assert.deepEqual(data, expectedResult);
+      if (cb) {
+        var err = null;
+        cb(err, this);
+      }
+    }
+  };
+};
+
